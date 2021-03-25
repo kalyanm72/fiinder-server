83760b80983d3d014b4fc045797e4d514e4e97d6
kalyan <pandukalyan2015@gmail.com>
2021-03-25 18:17:45 +0530

    /

--- app.js
+++ app.js
@@ -44,11 +44,11 @@ app.use(cookieParser());
 
 app.use(express.static(`${__dirname}/public`));
 
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
-    
-});
+// app.use(function(req,res,next){
+//     res.header("Access-Control-Allow-Origin","*");
+//     res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+
+// });
 
 app.use('/api/v1/posts', postrouter);
 
